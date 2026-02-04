f0a1fb9f18f99c6d7dea25c86f70722365912e33
Rodolfo Coyote <rodolfo.coyote@gameplanet.com>
2026-02-04 13:57:28 -0600

    test. fix. no validar ssl en windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'dart:io';
+
 import 'package:distribucionesgp/api/api_service.dart';
 import 'package:distribucionesgp/provider/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'app/app.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() async {
+  HttpOverrides.global = MyHttpOverrides();
   WidgetsFlutterBinding.ensureInitialized();
 
   final authProvider = AuthProvider();
