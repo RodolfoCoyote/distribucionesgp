6dc86c3239ad316db6eca19b218d06465c60f873
Rodolfo Coyote <rodolfo.coyote@gameplanet.com>
2026-02-04 13:20:05 -0600

    cambio cualquiera

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,7 +3,7 @@ class AppConstants {
   static const String appName = 'Distribuciones GP';
   static const String defaultLanguage = 'es';
   static const String apiBaseUrl = 'https://api.gameplanet.com/epod5';
-
+  static const String apiTimeout = '30'; // segundos
   //-- lo tomamos del .env
   static const String jwtMaster = String.fromEnvironment('JWT_TOKEN');
 }
